fe05088ca9f9bae595c182a095fd6d683a172f14
Aurélien <aurelien.strimelle@bstorm.be>
2023-02-16 13:42:49 +0100

    Add header

---
+++ src/containers/header/header.jsx
@@ -0,0 +1,9 @@
+import style from './header.module.css';
+
+const Header = () => (
+    <header className={style.main}>
+        <p>Exo 05 - Todo list</p>
+    </header>
+)
+
+export default Header;
