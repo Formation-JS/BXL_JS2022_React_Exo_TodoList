3817b70880347f2f50f58cdf02ca76a32004b668
Aurélien <aurelien.strimelle@bstorm.be>
2023-02-17 08:41:47 +0100

    Update Design 🧙‍♀️

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Header from './containers/header/header';
 import TodoApp from './containers/todo-app/todo-app';
 
 function App() {
-  return (<>
+  return (
+  <div className='Layout'>
     <Header />
-    <div className="App">
+    <div className='Main'>
       <h1>Todo App</h1>
       <TodoApp />  
     </div>
-  </>);
+  </div>);
 }
 
 export default App;
